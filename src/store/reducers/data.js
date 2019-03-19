f99f3baefdc687f5a44a205b7e39c54f38212602
AnuragTiwari1 <anuragkumartiwari79@gmail.com>
2019-03-19 16:59:28 +0530

    removed useless codes

--- src/store/reducers/data.js
+++ src/store/reducers/data.js
@@ -1,46 +1,8 @@
-import {
-  BOOKIE_LIST,
-  LEAGUE_LIST,
-  SNACK,
-  SUBSCRIPTION,
-  UNSUBSCRIBE,
-} from '../constants';
-
 const initialState = {
   leagueList: [],
-  bookieList: [],
-  snackContent: false,
-  subscriptionList: [],
 };
 function userReducer(state = initialState, { type, payload }) {
   switch (type) {
-    case LEAGUE_LIST:
-      return {
-        ...state,
-        leagueList: JSON.parse(payload),
-      };
-    case BOOKIE_LIST:
-      return {
-        ...state,
-        bookieList: JSON.parse(payload),
-      };
-    case SNACK:
-      return {
-        ...state,
-        snackContent: payload,
-      };
-    case SUBSCRIPTION:
-      return {
-        ...state,
-        subscriptionList: payload.slice(0).reverse(),
-      };
-    case UNSUBSCRIBE:
-      return {
-        ...state,
-        subscriptionList: state.subscriptionList.filter(
-          ele => ele.id !== payload
-        ),
-      };
     default:
       return state;
   }
--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,16 +1,9 @@
-import { SET_TOKEN } from '../constants';
-
 const initialState = {
   username: '',
   token: null,
 };
 function userReducer(state = initialState, { type, payload }) {
   switch (type) {
-    case SET_TOKEN:
-      return {
-        ...state,
-        token: payload,
-      };
     default:
       return state;
   }
